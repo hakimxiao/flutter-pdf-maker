029411c569ab0cf3c96f009d41cb0af6ca92dec6
hakimxiao <alhakimabusolihin@gmail.com>
2025-08-27 19:59:35 +0700

    Handle Error : Font disarankan menggunakan font yang kita siapkan sendiri
    Menambahkan Gambar pada PDF dengan Metode byte ke Uint8List

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
@@ -10,7 +11,7 @@ import 'package:pdf/widgets.dart' as pw;
 
 class HomeController extends GetxController {
   var product = List.generate(
-    5,
+    20,
     (index) => {
       'id': ' # ${index + 1}',
       'name': 'Product ke ${index + 1}',
@@ -19,44 +20,60 @@ class HomeController extends GetxController {
   );
 
   void getPDF() async {
+    // buat font
+    var dataFont = await rootBundle.load('assets/PlaywriteHU-Regular.ttf');
+    var myFont = pw.Font.ttf(dataFont);
+    //  buat gambar
+    var dataImage = await rootBundle.load('assets/image.jpg');
+    var myImage =
+        dataImage.buffer.asUint8List(); // converdari bytes ke Uint8List
+
     // 1. buat class pdf
     final pdf = pw.Document();
 
     // 2. buat page
     pdf.addPage(
-      pw.Page(
+      pw.MultiPage(
         pageFormat: PdfPageFormat.a4,
         build: (pw.Context context) {
-          return pw.Container(
-            width: double.infinity,
-            color: PdfColors.amber,
-            child: pw.Column(
-              crossAxisAlignment: pw.CrossAxisAlignment.start,
-              children: [
-                pw.Container(
-                  color: PdfColors.purple300,
-                  alignment: pw.Alignment.center,
-                  width: double.infinity,
-                  child: pw.Text(
-                    'My Products',
-                    style: pw.TextStyle(fontSize: 50),
-                  ),
+          return [
+            pw.ClipRRect(
+              horizontalRadius: 20,
+              verticalRadius: 20,
+              child: pw.Container(
+                height: 170,
+                width: 200,
+                child: pw.Image(
+                  // baca file Uint8List
+                  pw.MemoryImage(myImage),
+                  fit: pw.BoxFit.cover,
                 ),
-                pw.SizedBox(height: 20),
-                pw.Column(
-                  children:
-                      product
-                          .map(
-                            (e) => pw.Text(
-                              'ID : ${e['id']} - NAME : ${e['name']} - \n DESC : ${e['desc']} \n',
-                              style: pw.TextStyle(fontSize: 30),
-                            ),
-                          )
-                          .toList(),
-                ),
-              ],
+              ),
             ),
-          );
+            pw.SizedBox(height: 20),
+
+            pw.Container(
+              color: PdfColors.purple300,
+              alignment: pw.Alignment.center,
+              width: double.infinity,
+              child: pw.Text(
+                'My Products',
+                style: pw.TextStyle(fontSize: 50, font: myFont),
+              ),
+            ),
+            pw.SizedBox(height: 20),
+            pw.Column(
+              children:
+                  product
+                      .map(
+                        (e) => pw.Text(
+                          'ID : ${e['id']} - NAME : ${e['name']} - \n DESC : ${e['desc']} \n',
+                          style: pw.TextStyle(fontSize: 30, font: myFont),
+                        ),
+                      )
+                      .toList(),
+            ),
+          ];
         },
       ),
     ); // Page
